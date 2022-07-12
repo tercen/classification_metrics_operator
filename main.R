08514f1a8205e3e617572237275114c28cce6418
lucas501 <lucas.lelann@gmail.com>
2022-07-12 09:23:55 +0000

    add tim test and populate workflows

--- main.R
+++ main.R
@@ -1,7 +1,13 @@
 library(tercen)
 library(dplyr)
 
-df <- (ctx = tercenCtx())  %>% 
+#library(tim)
+#options("tercen.workflowId" = "6015a4dd34cef273755e1a1b1500427b")
+#options("tercen.stepId"     = "c51e5178-b2ae-4bda-80c5-ab8ed2c1edff")
+
+ctx <- tercenCtx()
+
+df <- ctx  %>% 
   select(.y, .ri) %>%
   mutate(predicted = ctx$select(ctx$colors[[1]])[[1]]) %>%
   mutate(truth = ctx$select(ctx$labels[[1]])[[1]])
@@ -13,9 +19,10 @@ precision <- diag(mat) / rowSums(mat)
 recall <- diag(mat) / colSums(mat)
 f1 <- 2*precision*recall / (precision+recall)
 
-df_out <- data.frame(label = names(precision), precision, recall, f1)
+df<- data.frame(label = names(precision), precision, recall, f1) 
+df_out<-ctx$addNamespace(df)
 
-table = tercen::dataframe.as.table(ctx$addNamespace(df_out)) 
+table = tercen::dataframe.as.table(df_out) 
 table$properties$name = 'value'
 table$columns[[1]]$type = 'double'
 
@@ -29,4 +36,6 @@ result = OperatorResult$new()
 result$tables = list(table)
 result$joinOperators = list(join)
 
-ctx$save(result) 
+ctx$save(result)
+
+#tim::build_test_data(res_table = df_out, ctx = ctx, test_name = "test1")
